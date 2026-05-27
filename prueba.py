4133565e91caeb9a38f64f5ebe34dc6ee660c774
drielijuan-dotcom <drieli.juan@gmail.com>
2026-05-27 12:05:05 +0200

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,5 @@
+edad = int(input("Por favor, introduce tu edad: "))
+if edad >= 18:
+    print("Acceso concedido! Eres mayor de 18 años")
+else:
+    print("Acceso denegado. No se permiten menores de 18 años.")
